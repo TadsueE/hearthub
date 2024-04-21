f9d6c4593c99cf8102093dfa0dc8493289802a02
TadsueE <jthads55@gmail.com>
2024-04-21 19:32:41 +0800

    3rd comit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ function App() {
       <button className="button-Aboutweb"> About the Web </button>
       <img src={header} className="logo-header" alt="logo" />
       </div>
+      <div className="body">
       <div className="Main-content">
       <img src={Heart} className="Main-heart" alt="Heart"/>
       <p className="Main-heading">Heart Check Hub: Stay Informed, Stay Healthy! </p>
       <p className="Main-Subheading">A simple heart Check up by using factors that can lead to cardio vascular diseases </p>
       <button className="button-start"> Get Started <img src={Arrow} className="Arrow" alt="arrow"/> </button>
+      </div>
       </div>
     </div>
   );
